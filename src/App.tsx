72bf96eb4fcd181501237fe4ef4a367637bd1b88
Volodymir Tymtsias <vovanuch2007@gmail.com>
2023-12-08 15:44:37 +0200

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Test
-      </header>
+      <Header />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider, createTheme } from '@mui/material';
+import './normalize.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#87bfff'
+    },
+    secondary: {
+      main: '#f6d371'
+    },
+  }
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
